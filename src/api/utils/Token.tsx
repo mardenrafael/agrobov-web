1efbd0d4b397d6a6f3f36af3db98d8d7f66e5188
magnojunior07 <magnojunior319@gmail.com>
2022-10-16 20:58:25 -0400

    add token for test conection

---
+++ src/api/utils/Token.tsx
@@ -0,0 +1,3 @@
+const TOKEN ="Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjY1OTMyMDY3LCJleHAiOjE2NjYwMTg0Njd9.02JWCUy-oSqzBKNVsSzBILowvotKqte5JkKYwMP8aAM";
+
+export default  TOKEN
