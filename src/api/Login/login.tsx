459508d419dc47e652612eac0f8e6df7181c7fc6
magnojunior07 <magnojunior319@gmail.com>
2022-10-20 19:49:59 -0400

    add login fetch data

--- src/api/Login/login.tsx
+++ src/api/Login/login.tsx
@@ -1,3 +1,71 @@
-import axios from "axios";
-import react from "react"
+import react, { PropsWithChildren } from "react";
 
+export default class LoginForm extends react.Component {
+    constructor(props: PropsWithChildren) {
+        super(props);
+        this.state = {
+            email: "",
+            password: ""
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this)
+    };
+
+    async getAuthorizationToken(data: FormData) {
+        const TOKEN = await fetch("http://localhost:4000/login", {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+            "Content-Type": "application/json"
+            }
+        }).then((res) => {
+            return res.json()
+        })
+        
+        const TOKEN_WITH_BEARER = `Bearer ${TOKEN.token}`
+
+        console.log(TOKEN_WITH_BEARER);
+        
+        return TOKEN_WITH_BEARER
+        
+    }
+
+    handleChange(event) {
+    event.preventDefault()
+
+    const { name, value }= event.target        
+    
+    this.setState({
+            [name]: value 
+        })
+
+    }
+
+    handleSubmit() {
+        
+        const TOKEN = this.getAuthorizationToken(this.state);
+
+        console.log(TOKEN);
+        
+    }
+
+    render(): react.ReactNode {
+        return(
+            <>
+                <label htmlFor="email">
+                    Email:
+                    <input type="email" name="email" id="email" value={this.state.email} onChange={this.handleChange}/>    
+                </label> 
+                <br />   
+                <label htmlFor="password">
+                    Senha:
+                    <input type="password" name="password" id="password" value={this.state.password} onChange={this.handleChange}/>
+                </label>
+                <br />
+                <button onClick={this.handleSubmit}>
+                    login    
+                </button>           
+            </>
+        )
+    }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from "next";
 import User from "../api/User/GetUser";
+import Login from "../api/Login/login";
 
 const Home: NextPage = () => {
   return (
-   <User />
+   <Login />
   );
 };
 
