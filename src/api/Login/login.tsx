e324ec1abf40a3db234be2bc6488deba64777656
magnojunior07 <magnojunior319@gmail.com>
2022-10-25 12:14:17 -0400

    transform login class in a function

--- src/api/Login/login.tsx
+++ src/api/Login/login.tsx
@@ -1,17 +1,17 @@
-import react, { PropsWithChildren } from "react";
+import { redirect } from "next/dist/server/api-utils";
+import react, { PropsWithChildren, useState } from "react";
+import User from "../User/GetUser";
 
-export default class LoginForm extends react.Component {
-    constructor(props: PropsWithChildren) {
-        super(props);
-        this.state = {
-            email: "",
-            password: ""
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this)
-    };
+export default function LoginForm() {
+       const [email, setEmail] = useState("")
+       const [password, setPassword] = useState("")
 
-    async getAuthorizationToken(data: FormData) {
+       const data = {
+        email: email,
+        password: password
+       }
+    
+    async function getAuthorizationToken(data: FormData) {
         const TOKEN = await fetch("http://localhost:4000/login", {
             method: "POST",
             body: JSON.stringify(data),
@@ -30,42 +30,46 @@ export default class LoginForm extends react.Component {
         
     }
 
-    handleChange(event) {
+    async function handleChange(event) {
     event.preventDefault()
 
     const { name, value }= event.target        
+    if(name =="email") {
+        setEmail(value)
+    } else {
+        setPassword(value)
+    }
+
+    console.log(data);
     
-    this.setState({
-            [name]: value 
-        })
-
     }
 
-    handleSubmit() {
+    function handleSubmit() {
+        const TOKEN = getAuthorizationToken(data);
         
-        const TOKEN = this.getAuthorizationToken(this.state);
-
-        console.log(TOKEN);
+        return(
+            <>
+                <User token={TOKEN}></User>
+            </>
+        )
         
     }
 
-    render(): react.ReactNode {
         return(
             <>
                 <label htmlFor="email">
                     Email:
-                    <input type="email" name="email" id="email" value={this.state.email} onChange={this.handleChange}/>    
+                    <input type="email" name="email" id="email" value={data.email} onChange={handleChange}/>    
                 </label> 
                 <br />   
                 <label htmlFor="password">
                     Senha:
-                    <input type="password" name="password" id="password" value={this.state.password} onChange={this.handleChange}/>
+                    <input type="password" name="password" id="password" value={data.password} onChange={handleChange}/>
                 </label>
                 <br />
-                <button onClick={this.handleSubmit}>
+                <button onClick={handleSubmit}>
                     login    
                 </button>           
             </>
         )
-    }
 }
