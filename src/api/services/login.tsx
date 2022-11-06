06e166a1d27e73b4b5268f15e68786b4b0f210b3
magnojunior07 <magnojunior319@gmail.com>
2022-11-05 22:34:42 -0400

    add login service

---
+++ src/api/services/login.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import User from "../User/User";
+import BASE_URL from "../utils/BaseUrl";
+
+export default async function GetLogin(email: string | undefined, password: string | undefined) {
+       const data = {
+        email: email,
+        password: password
+       }
+
+       const TOKEN = await fetch(`${BASE_URL}/login`, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+            "Content-Type": "application/json"
+            }
+        }).then((res) => {
+            return res.json()
+        })
+        
+    const TOKEN_WITH_BEARER = `Bearer ${TOKEN.token}`
+    console.log(TOKEN_WITH_BEARER);
+       
+    return TOKEN_WITH_BEARER;
+}
